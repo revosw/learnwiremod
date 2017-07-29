106be6eba0fa7799e13a0f5167c3d238b4fbdfd4
revosw <revosw@gmail.com>
2017-07-29 13:08:37 +0200

    Testing offline experience

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,11 @@
-hljs.initHighlightingOnLoad()
+if ('serviceWorker' in navigator) {
+
+  navigator.serviceWorker
+    .register('/serviceworker.js', { scope: '/' })
+    .then(function(registration) {
+      console.log("Service Worker Registered");
+    })
+    .catch(function(err) {
+      console.log("Service Worker Failed to Register", err);
+    })
+}
