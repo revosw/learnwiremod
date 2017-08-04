53ba0dd9eba366a1023ea9bf50a24948e6244582
revosw <revosw@gmail.com>
2017-08-04 18:04:06 +0200

    Sidenav now using jQuery

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,31 +13,13 @@
 //Run  function  when  the page's content has loaded
 window.onload = function() {
 
-
-
-  /*console.log("Inside IIFE");
-  var tabs = document.querySelectorAll(".tab");
-  console.log(tabs)
-  var sidenav = document.querySelector("#sidenav");
-  for(tab of Array.from(tabs)) {
-    console.log(tab)
-    tab.addEventListener("click", e => {
-      if(e.target.classList.toString() === "tab active") return;
-
-      document.querySelector(".active").classList.remove("active")
-      e.target.classList.add("active");
-      
-      if(document.querySelector(".active").innerText === "Tutorials") {
-        console.log("Changed 1")
-        document.querySelector(".tutlist").style.display = "";
-        document.querySelector(".reflist").style.display = "none";
-      } else {
-        console.log("Changed 2")
-        document.querySelector(".tutlist").style.display = "none";
-        document.querySelector(".reflist").style.display = "";
-      }
-      sidenav.classList.remove(sidenav.classList.contains("tutorials") ? "reference" : "tutorials");
-      sidenav.classList.add(e.target.innerText.toLowerCase());
+  $("#sidenav").load("/assets/includes/sidenav.html", () => {
+    $(".tab").on("click", e => {
+      if ( $(e.target).hasClass("active") ) return;
+      $(".tab").toggleClass("active");
+      $("#sidenav").toggleClass("tutorials reference");
+      $(".tutlist").toggle();
     });
-  }*/
-}
+  });
+
+};
