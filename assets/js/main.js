e3892347aef3afb058d2664918e9bdb9159f8b94
revosw <revosw@gmail.com>
2017-08-06 01:58:12 +0200

    Made website responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,6 +13,8 @@
 //Run  function  when  the page's content has loaded
 window.onload = function() {
 
+  $("h1").on("click", () => console.log("clickedd"))
+
   $("#sidenav").load("/assets/includes/sidenav.html", () => {
     $(".tab").on("click", e => {
       if ( $(e.target).hasClass("active") ) return;
@@ -23,14 +25,25 @@ window.onload = function() {
 
     $("li > a:first-child").on("click", e => {
       $.ajax({
-        url: `/e2/${e.target.innerText.replace(/ /g,"-").toLowerCase()}.html`,
+        url: `${location.pathname.replace(".html","")}/${e.target.innerText.replace(/ /g,"-").toLowerCase()}.html`,
         success: data => {
           $("main").html(data)
+
+          
           //Initialize highlighting for code blocks when changing page
           $("code").html(hljs.highlightAuto($("code").text()).value)
         }
       })
     })
-  })
 
+    $(".sidetoggle").on("click", e => {
+      var togg = $("#sidenav")[0].style.left == "0px" ? "-430px" : "0px"
+      $("#sidenav").animate({ left: togg })
+
+      console.log(togg)
+      $("body::before").animate({ background: "rgba(0,0,0,0.6)" })
+    })
+
+
+  })
 };
