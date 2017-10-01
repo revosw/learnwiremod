3e57d038135db9ce9e95314de8f642ffb1ecd03b
revosw <revosw@gmail.com>
2017-10-01 18:53:58 +0200

    Changed layout. All Varela Round fonts are changed to Raleway.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,10 @@
 "use strict";
+// I am not sure if I want to add a service worker. Service worker is to
+// make the website a PWA (Progressive web app), in which I can make the
+// website app-like, in the form of being able to add the website to your
+// home screen really easily on mobile, and even being able to use the
+// website without an internet connection.
+// https://developers.google.com/web/progressive-web-apps/
 /*if ('serviceWorker' in navigator) {
 
   navigator.serviceWorker
@@ -16,12 +22,23 @@ function kebabCase(text) {
   return text.replace(/\s+/g, "-").toLowerCase();
 }
 
-function changePageOrSection(event) {
+/*function generateNextButton() {
+  // The next button should be appended to the bottom of every page, inside <main>
+  // It should contain the name of the next page, which is dynamically fetched.
 
+  var main = document.querySelector("main")
+  var button = document.createElement("a")
+
+  button.className = "next button"
+  button.innerHTML = `<img src="/assets/img/arrow.png">`
+  main.appendChild(button)
+}*/
+
+function changePageOrSection(event) {
 
   // Since the sidenav contains both links that refer to parent pages, and sublinks that refers to headers
   // inside the parent pages, we have to differentiate between the two. Since we cannot select the first sibling
-  // element from a sibling selector, we need to traverse up to the parent, then select the parent's first child.
+  // element from a sibling selector, we need to traverse up to the parent, then select the parent's :first-child.
   var parentPageElem = $(event.target).parents("li").children(":first-child")[0];
 
   var parentPageLink = kebabCase( parentPageElem.innerText );
@@ -30,20 +47,19 @@ function changePageOrSection(event) {
   
   
   // We don't want to traverse to another page if we are already on the selected parent page
+  // location.search.substring(6) returns the value just after "?page="
   if (parentPageLink === location.search.substring(6)) {
-    //And we don't want to do anything if we have clicked the same link as we are aleady on
+    // And we don't want to do anything if we have clicked the same link as we are already on
     if (parentPageElem === event.target) return location.hash = "";
     
     // PROBLEM RIGHT HERE - Changing location.hash brings you back to default page
     location.hash = kebabCase(clickedLinkText);
     return;
-  }
+  };
 
   // But if the selected parent page is different, we need to know what parent page to fetch.
   // Is the clicked link a part of tutorials or reference? We have two directories, one for each
-  var tutOrRef = $(event.target).parents("ul").hasClass("tutlist")
-    ? "tutorials"
-    : "reference";
+  var tutOrRef = $(event.target).parents("ul").attr("class");
 
   importer.load(
     "main",
@@ -51,11 +67,12 @@ function changePageOrSection(event) {
     // Initialize highlighting for code blocks after loading page
     function () {
       highlightCode();
+      /*generateNextButton();*/
       //setTimeout(() => location.hash = subLinkText,10000)
     },
     `${parentPageLink}`
   );
-}
+};
 
 
 function highlightCode() {
@@ -64,7 +81,7 @@ function highlightCode() {
   }
 }
 
-//Run  function  when  the page's content has loaded
+// Run function when the page's content has loaded
 window.onload = function() {
   importer.load("#sidenav","sidenav.html", () => {
 
@@ -73,25 +90,16 @@ window.onload = function() {
     for(var page of $("#sidenav ul a:first-child")) {
       var pageName = kebabCase(page.innerText);
 
-      pages[pageName] = $(page).parents("ul").hasClass("tutlist")
-      ? `tutorials/${pageName}.html`
-      : `reference/${pageName}.html`;
-    }
+      pages[pageName] = `${$(page).parents("ul").attr("class")}/${pageName}.html`
+    };
     
     importer.initialize(
       "main",
       pages,
-      function() { highlightCode(); },
+      function() { highlightCode(); /*generateNextButton();*/ },
       `introduction`,
       function() { highlightCode(); },
     );
-
-    $(".tab").click( e => {
-      if ( $(e.target).hasClass("active") ) return;
-      $(".tab").toggleClass("active");
-      $("#sidenav").toggleClass("tutorials reference");
-      $(".tutlist").toggle();
-    });
     
     $("#sidenav ul li a").click( e => changePageOrSection(e) );
 
