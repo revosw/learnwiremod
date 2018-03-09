110adf4223aaf222aa7dfb2ba3bec4887a0eb690
Revolution <revosw@gmail.com>
2018-03-09 12:23:10 +0100

    Added tutorial content for e2

--- js/main.js
+++ js/main.js
@@ -16,37 +16,3 @@
       console.log("Service Worker Failed to Register", err);
     })
 }*/
-
-// Run function when the page's content has loaded
-window.onload = function() {
-
-  // Have to reset the sidenav to show it when expanding the window, and hide at mobile
-  $(window).on("resize", () => {
-
-    // If the viewport's (the browser window) width is less than 1200px, hide the sidenav
-    $("#sidenav")[0].style.left = window.innerWidth < 1200
-      ? "-430px"
-      : "0px";
-  });
-
-  // Just to toggle the sidenav. Real simple. The button for toggling sidenav is hidden at largest breakpoint
-  $(".sidetoggle").click( () => {
-    var toggle = $("#sidenav")[0].style.left == "0px"
-      ? "-430px"
-      : "0px";
-
-    $("#sidenav").animate({ left: toggle });
-  });
-
-  // It's nice when you can just click anywhere outside of the sidenav to hide it when viewing
-  // the page on mobile
-  $("main").click( () => {
-    if (window.innerWidth <= 1200) $("#sidenav").animate({ left: "-430px" });
-  })
-
-  // The quiz module will be present on many of the pages
-  var quiz = document.querySelector(`section.quiz`)
-  if (quiz) {
-    
-  }
-}
