95d39cd577fbbb11e6bc61b3db2cc17c6be3a32d
revosw <revosw@gmail.com>
2021-01-08 20:44:24 +0100

    CodeCopy, highlight rules, new e2 content

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,6 @@
 import hljs from "./lib/highlight.js"
 import expression2 from "./lib/expression2.js"
+import CodeCopy from "./lib/codecopy.js";
 import ScrollSpy from "./lib/ScrollSpy.js";
 import Quizlet from "./components/Quizlet.js";
 
@@ -7,5 +8,6 @@ document.addEventListener("DOMContentLoaded", () => {
     ScrollSpy("#navMain", {offset: 4});
     customElements.define("lw-quizlet", Quizlet);
     hljs.registerLanguage("expression2", expression2)
+    hljs.addPlugin({"before:highlightBlock": CodeCopy});
     hljs.initHighlightingOnLoad();
 })
--- public/js/lib/expression2.js
+++ public/js/lib/expression2.js
@@ -1,10 +1,33 @@
-const TYPES = /angle|array|bone|complex|effect|entity|gtable|matrix[24]?|normal|number|quaternion|ranger|string|table|vector[24]?|void|wirelink/;
-
 export default function (hljs) {
     return {
-        keywords: "for if while local function #include",
         name: "expression2",
         contains: [
+            {
+                className: "keyword",
+                match: /if|elseif|else|for|foreach|while|continue|break|switch|case|default|local|return|#include/
+            },
+            {
+                begin: /function/, end: /^/,
+                returnBegin: true,
+                contains: [
+                    {
+                        className: "keyword",
+                        match: /function/
+                    },
+                    {
+                        className: "type",
+                        match: TYPELIST
+                    },
+                    {
+                        className: "udf",
+                        match: /\w+(?=\()/
+                    },
+                    {
+                        className: "variable",
+                        match: /[A-Z]\w*/
+                    }
+                ]
+            },
             {
                 className: "number",
                 variants: [
@@ -14,31 +37,34 @@ export default function (hljs) {
                 ],
             },
             {
+                className: "preprocess",
+                match: /#ifdef|#ifndef|#else|#endif/
+            },
+            {
                 className: "string",
                 begin: /"/, end: /(?<!\\)"/
             },
             {
                 className: "directive",
-                match: /@autoupdate|@trigger|@name( [^\n]+)?|@model( [^\n]+)?/
-            },
-            {
-                className: "directive",
-                match: /@inputs|@outputs|@persist/,
-                contains: [
-                    {
-                        className: "type",
-                        match: TYPES
-                    }
-                ]
+                match: /@autoupdate|@inputs|@outputs|@persist|@trigger( all| none)?|@name( [^\n]+)?|@model( [^\n]+)?/
             },
             {
                 className: "variable",
                 match: /[A-Z]\w*/
             },
+            {
+                className: "type",
+                variants: [
+                    {match: "(?<=:)" + TYPELIST},
+                    {match: TYPELIST + "(?=:)"},
+                    {match: TYPELIST + "(?=\])"}
+                ]
+            },
             hljs.HASH_COMMENT_MODE,
             {
                 className: "comment",
-                begin: /#\[/, end: /]#/
+                begin: /#\[/m, end: /]#/m
+
             },
             {
                 className: "function",
@@ -47,3 +73,5 @@ export default function (hljs) {
         ]
     }
 }
+
+const TYPELIST = "(angle|array|bone|complex|effect|entity|gtable|matrix[24]?|normal|number|quaternion|ranger|string|table|vector[24]?|void|wirelink)";
